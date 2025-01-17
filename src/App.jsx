99efdfb99cf9ce5ecc9dd4a83871233c777ec85e
GeoffJop0908 <geoffjop0908@gmail.com>
2025-01-17 23:27:43 +0800

    server name changed with dropdown-hover

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,29 @@ export default function App() {
 
 function Servers(props) {
   return (
-    <div className="bg-[#212226] w-18 p-2 space-y-3">
+    <div className="bg-[#212226] max-w-18 p-2 gap-3 flex flex-col">
       {images.map((image, index) => (
-        <button
-          key={index}
-          className="size-14 bg-gray-600 flex items-center justify-center rounded-full"
-        >
-          <div className="avatar tooltip tooltip-right" data-tip="hello">
-            <div className="w-fit rounded-badge hover:rounded-2xl transition-all duration-200">
-              <img src={`./src/assets/${image}`} alt={`Server ${index + 1}`} />
+        <div className="dropdown dropdown-hover dropdown-right dropdown-center">
+          <button
+            key={index}
+            className="size-14 bg-gray-600 flex items-center justify-center rounded-full"
+          >
+            <div className="avatar">
+              <div className="h-full rounded-badge hover:rounded-2xl transition-all duration-200">
+                <img
+                  src={`./src/assets/${image}`}
+                  alt={`Server ${index + 1}`}
+                />
+              </div>
             </div>
+          </button>
+          <div
+            tabIndex={0}
+            className="dropdown-content rounded-lg z-[1] p-2 shadow translate-x-2 max-w-48 whitespace-pre bg-black text-white"
+          >
+            <div className="w-full">Server Name</div>
           </div>
-        </button>
+        </div>
       ))}
     </div>
   );
