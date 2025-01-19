a4eb0d20b01f14de30a0a0b1593808731ab13d57
GeoffJop0908 <geoffjop0908@gmail.com>
2025-01-19 15:08:02 +0800

    more overflow-ellipsis stuff

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -47,28 +47,32 @@ export default function Category(props) {
   }
 
   return (
-    <div className="pl-4 collapse gap-2">
+    <div className="pl-4 collapse gap-2 overflow-hidden">
       <input type="checkbox" defaultChecked className="h-5 min-h-5" />
-      <div className="w-full text-left text-white font-bold uppercase collapse-title p-0 min-h-5 h-5 flex ">
+      <div className="w-full text-left text-white font-bold uppercase collapse-title p-0 min-h-5 h-5 flex min-w-0">
         <p className="translate-x-[-5px] translate-y-[-4px]">
           <i className="arrow down size-2"></i>
         </p>
-        <p className="grow">{props.title}</p>
+        <p className="grow overflow-ellipsis whitespace-nowrap overflow-hidden">
+          {props.title}
+        </p>
         <AddChannel currentCategory={props.currentCategory} />
       </div>
-      <div className="collapse-content pl-0 !pb-0 pr-0">
+      <div className="collapse-content pl-0 !pb-0 pr-0 w-full overflow-hidden">
         {props.tabChannels.map((channel, index) => (
           <button
             key={index}
             onClick={() => channelClick(index, channel)}
-            className={`w-full text-left flex items-center space-x-2 pl-4 rounded-md ${
+            className={`w-full text-left flex items-center space-x-2 pl-4 rounded-md min-w-0 ${
               isActiveChannel == index
                 ? 'bg-opacity-10 bg-gray-50 text-white'
                 : ' text-gray-400 hover:text-white '
             }`}
           >
             <span className="text-2xl">#</span>
-            <span className="overflow-hidden w-full h-6">{channel.name}</span>
+            <span className="w-full h-6 overflow-ellipsis whitespace-nowrap overflow-hidden">
+              {channel.name}
+            </span>
           </button>
         ))}
       </div>
